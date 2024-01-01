d0f368b217aa7136abf4e948d46267f3f3d1c11b
Your Name <you@example.com>
2024-01-01 17:42:16 +0530

    Update

--- pages/js/auth.js
+++ pages/js/auth.js
@@ -1,136 +1,155 @@
-    // Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-    import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-    import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-    import { sendEmailVerification } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { sendEmailVerification } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-        apiKey: "AIzaSyBglkIqOIMsIIpnJBZtfQSRDdgRSXm-DPU",
-        authDomain: "studygalaxy-8aa56.firebaseapp.com",
-        projectId: "studygalaxy-8aa56",
-        storageBucket: "studygalaxy-8aa56.appspot.com",
-        messagingSenderId: "657368593724",
-        appId: "1:657368593724:web:ee0b1facca005fe9689a39"
-    };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBglkIqOIMsIIpnJBZtfQSRDdgRSXm-DPU",
+  authDomain: "studygalaxy-8aa56.firebaseapp.com",
+  projectId: "studygalaxy-8aa56",
+  storageBucket: "studygalaxy-8aa56.appspot.com",
+  messagingSenderId: "657368593724",
+  appId: "1:657368593724:web:ee0b1facca005fe9689a39",
+};
 
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore();
-    const auth = getAuth(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+const auth = getAuth(app);
 
-    let email = document.getElementById('email');
-    let emailreg = document.getElementById('emailreg');
-    let password = document.getElementById('password');
-    let passwordreg = document.getElementById('passwordreg');
-    let loginForm = document.getElementById('login');
-    let signupForm = document.getElementById('signup');
-    let username = document.getElementById('username');
+let email = document.getElementById("email");
+let emailreg = document.getElementById("emailreg");
+let password = document.getElementById("password");
+let passwordreg = document.getElementById("passwordreg");
+let loginForm = document.getElementById("login");
+let signupForm = document.getElementById("signup");
+let username = document.getElementById("username");
 
+let registerUser = async (evt) => {
+  evt.preventDefault();
 
-    let registerUser = async evt => {
-        evt.preventDefault();
-        
-        try {
-            const credentials = await createUserWithEmailAndPassword(auth, emailreg.value, passwordreg.value);
-    
-            // Send verification email
-            await sendVerificationEmail(credentials.user);
-    
-            // Increment user counter
-            const userCounterRef = doc(db, "UserAuthList", "userCounter");
-            const counterDoc = await getDoc(userCounterRef);
-            let userCount = counterDoc.exists() ? counterDoc.data().count + 1 : 1;
-            
-            // Set user registration details with the incremented counter
-            const userRef = doc(db, "UserAuthList", credentials.user.uid);
-            await setDoc(userRef, {
-                Name: username.value,
-                email: emailreg.value,
-                regNo: userCount,
-            });
-    
-            // Update the user counter in the database
-            await setDoc(userCounterRef, { count: userCount });
-    
-            Swal.fire({
-                title: "Success!",
-                text: "Your account has been created. Please check your email for verification.",
-                icon: "success"
-            });
-    
-            console.log(credentials);
-        } catch (error) {
-            Swal.fire({
-                title: "Error!",
-                text: error.message,
-                icon: "error"
-            });
-    
-            console.error(error.code);
-            console.error(error.message);
-        }
-    };
-    
-    
-    // Send verification email function
-    async function sendVerificationEmail(user) {
-        try {
-            await sendEmailVerification(user);
-            console.log("Verification email sent!");
-        } catch (error) {
-            console.error("Error sending verification email:", error.message);
-            throw error; // Rethrow the error to be caught in the main registerUser catch block
-        }
-    };
-    
-    let signInUser = async evt => {
-        evt.preventDefault();
-        const emailValue = email.value;
-        const passwordValue = password.value;
-    
-        try {
-            const credentials = await signInWithEmailAndPassword(auth, emailValue, passwordValue);
-    
-            // Check if the email is verified
-            if (credentials.user.emailVerified) {
-                // Proceed with sign-in
-                Swal.fire({
-                    html: `<span class="spinner-border text-dark spinner-border-sm" role="status" aria-hidden="true"></span> Logging in...`
-                });
-    
-                const ref = doc(db, "UserAuthList", credentials.user.uid);
-                const docSnap = await getDoc(ref);
-    
-                if (docSnap.exists()) {
-                    localStorage.setItem("user-info", JSON.stringify({
-                        Name: docSnap.data().Name,
-                    }));
-                    localStorage.setItem("user-creds", JSON.stringify(credentials.user));
-                    window.location.href = "studygalaxy.html";
-                }
-            } else {
-                // Show verification prompt
-                Swal.fire({
-                    title: "Verify!",
-                    text: "Please verify your email address",
-                    icon: "info"
-                });
-            }
-        } catch (error) {
-            Swal.fire({
-                title: "Error!",
-                text: error.message,
-                icon: "error"
-            });
-            console.log(error.code);
-            console.log(error.message);
-        }
-    };
-    
-   
-    loginForm.addEventListener('submit', signInUser);
-    signupForm.addEventListener('submit', registerUser);
+  try {
+    const credentials = await createUserWithEmailAndPassword(
+      auth,
+      emailreg.value,
+      passwordreg.value
+    );
+
+    // Send verification email
+    await sendVerificationEmail(credentials.user);
+
+    // Increment user counter
+    const userCounterRef = doc(db, "UserAuthList", "userCounter");
+    const counterDoc = await getDoc(userCounterRef);
+    let userCount = counterDoc.exists() ? counterDoc.data().count + 1 : 1;
+
+    // Set user registration details with the incremented counter
+    const userRef = doc(db, "UserAuthList", credentials.user.uid);
+    await setDoc(userRef, {
+      Name: username.value,
+      email: emailreg.value,
+      regNo: userCount,
+    });
+
+    // Update the user counter in the database
+    await setDoc(userCounterRef, { count: userCount });
+
+    Swal.fire({
+      title: "Success!",
+      text: "Your account has been created. Please check your email for verification.",
+      icon: "success",
+    });
+
+    console.log(credentials);
+  } catch (error) {
+    Swal.fire({
+      title: "Error!",
+      text: error.message,
+      icon: "error",
+    });
+
+    console.error(error.code);
+    console.error(error.message);
+  }
+};
+
+// Send verification email function
+async function sendVerificationEmail(user) {
+  try {
+    await sendEmailVerification(user);
+    console.log("Verification email sent!");
+  } catch (error) {
+    console.error("Error sending verification email:", error.message);
+    throw error; // Rethrow the error to be caught in the main registerUser catch block
+  }
+}
+
+let signInUser = async (evt) => {
+  evt.preventDefault();
+  const emailValue = email.value;
+  const passwordValue = password.value;
+
+  try {
+    const credentials = await signInWithEmailAndPassword(
+      auth,
+      emailValue,
+      passwordValue
+    );
+
+    // Check if the email is verified
+    if (credentials.user.emailVerified) {
+      // Proceed with sign-in
+      Swal.fire({
+        html: `<span class="spinner-border text-primary spinner-border-sm" role="status" aria-hidden="true"></span> Logging in...`,
+
+        showConfirmButton: false,
+      });
+
+      const ref = doc(db, "UserAuthList", credentials.user.uid);
+      const docSnap = await getDoc(ref);
+
+      if (docSnap.exists()) {
+        localStorage.setItem(
+          "user-info",
+          JSON.stringify({
+            Name: docSnap.data().Name,
+          })
+        );
+        localStorage.setItem("user-creds", JSON.stringify(credentials.user));
+        window.location.href = "studygalaxy.html";
+      }
+    } else {
+      // Show verification prompt
+      Swal.fire({
+        title: "Verify!",
+        text: "Please verify your email address",
+        icon: "info",
+      });
+    }
+  } catch (error) {
+    Swal.fire({
+      title: "Error!",
+      text: error.message,
+      icon: "error",
+    });
+    console.log(error.code);
+    console.log(error.message);
+  }
+};
+
+loginForm.addEventListener("submit", signInUser);
+signupForm.addEventListener("submit", registerUser);
---
+++ pages/solo/script.js
@@ -0,0 +1,217 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBglkIqOIMsIIpnJBZtfQSRDdgRSXm-DPU",
+    authDomain: "studygalaxy-8aa56.firebaseapp.com",
+    databaseURL: "https://studygalaxy-8aa56-default-rtdb.firebaseio.com",
+    projectId: "studygalaxy-8aa56",
+    storageBucket: "studygalaxy-8aa56.appspot.com",
+    messagingSenderId: "657368593724",
+    appId: "1:657368593724:web:ee0b1facca005fe9689a39",
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  let UserCreds = JSON.parse(localStorage.getItem("user-creds"));
+  let UserInfo = JSON.parse(localStorage.getItem("user-info"));
+  let unsavedData = JSON.parse(localStorage.getItem("unsavedData"));
+  if (localStorage.getItem("unsavedData")){
+    firebase
+    .database()
+    .ref("studytime")
+    .child(UserCreds.uid)
+    .update({
+      time: firebase.database.ServerValue.increment(unsavedData),
+    });
+  }
+  let checkCred = () => {
+    if (!localStorage.getItem("user-creds")) {
+      window.location.href = "/pages/Login.html";
+    }
+  };
+  window.addEventListener("load", checkCred);
+
+  div = document.querySelector(".nav").classList;
+  document.querySelector(".bi-gear").addEventListener("click", () => {
+    div.toggle("nav");
+    document.querySelector(".btn1").style.display = "none";
+  });
+  document.querySelector(".bt").addEventListener("click", () => {
+    div.toggle("nav");
+    document.querySelector(".btn1").style.display = "block";
+  });
+
+  async function randomQuote() {
+    const response = await fetch("https://api.quotable.io/random");
+    const quote = await response.json();
+    // Output the quote and author name
+    document.getElementById("quote").innerHTML = quote.content;
+    document.getElementById("quote-author").innerHTML = "-" + quote.author;
+  }
+  randomQuote();
+  const pomodoro = document.querySelector(".timer");
+  var time = 1500;
+  var Break = 300;
+  var clock = time;
+  var condition = true;
+  const startTimer = () => {
+    window.addEventListener("beforeunload", beforeUnloadHandler);
+    timer = setInterval(() => {
+      getScreenLock()
+      if (time === 3) {
+        // playAdudio("countdown.mp3");
+      }
+      if (time > 0) {
+        // progress(time);
+        time--;
+        const totalSeconds = time;
+
+        const minutes = Math.floor(totalSeconds / 60);
+        const seconds = totalSeconds % 60;
+
+        function padTo2Digits(num) {
+          return num.toString().padStart(2, "0");
+        }
+
+        result = `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`;
+        pomodoro.innerHTML = result;
+      } else if (condition === false) {
+        window.removeEventListener("beforeunload", beforeUnloadHandler);
+        clearInterval(timer);
+        condition = true;
+        time = clock;
+        startTimer();
+        Swal.fire({
+          title: "your brake is over",
+          text: "have a great study session",
+          icon: "info",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      } else {
+        window.removeEventListener("beforeunload", beforeUnloadHandler);
+        firebase
+          .database()
+          .ref("studytime")
+          .child(UserCreds.uid)
+          .update({
+            time: firebase.database.ServerValue.increment(clock),
+          });
+        time = clock;
+        const swalWithBootstrapButtons = Swal.mixin({
+          customClass: {
+            confirmButton: "btn btn-success",
+            cancelButton: "btn btn-primary",
+          },
+          buttonsStyling: false,
+        });
+        swalWithBootstrapButtons
+          .fire({
+            title: "Good Work!",
+            text: "Scession is over",
+            icon: "success",
+            showCancelButton: true,
+            confirmButtonText: "take a break",
+            cancelButtonText: "Continue studying",
+            backdrop: `#212529bf`,
+          })
+          .then((result) => {
+            if (result.isConfirmed) {
+              swalWithBootstrapButtons.fire({
+                title: "Break Started",
+                text: "Drink some water and stay hydrated",
+                icon: "info",
+                showConfirmButton: false,
+                timer: 2000,
+              });
+              condition = false;
+              time = Break;
+              startTimer();
+            } else if (
+              /* Read more about handling dismissals below */
+              result.dismiss === Swal.DismissReason.cancel
+            ) {
+              swalWithBootstrapButtons.fire({
+                title: "Session Started",
+                timer: 2000,
+                text: "You should take a break",
+                icon: "info",
+                showConfirmButton: false,
+              });
+
+              time = clock;
+              startTimer();
+            }
+          });
+        clearInterval(timer);
+      }
+    }, 1000);
+  };
+
+  let isTimerRunning = false;
+  let isReloading = false;
+  pomodoro.addEventListener("click", () => {
+    if (isTimerRunning) {
+      clearInterval(timer); // Pause the timer
+    } else {
+      startTimer(); // Start or resume the timer
+    }
+
+    isTimerRunning = !isTimerRunning;
+  });
+
+  function beforeUnloadHandler(event) {
+    isReloading = true;
+    // Show a confirmation message if needed
+    var confirmationMessage =
+      "if you are leaving your study time will be updated afeter you come back";
+    (event || window.event).returnValue = confirmationMessage;
+    return confirmationMessage;
+  }
+  window.addEventListener("unload", function () {
+    if (isReloading) {
+      sendDataToDatabase();
+    }
+  });
+
+  function sendDataToDatabase() {
+    // Add your code here to send data to the database
+    if (condition) {
+      localStorage.setItem("unsavedData", clock - time);
+    }
+  }
+  setTimeout(() => {
+    localStorage.setItem("unsavedData", 0);
+  }, 100);
+
+  document.getElementById("quit").addEventListener("click", () => {
+    if (condition) {
+      window.removeEventListener("beforeunload", beforeUnloadHandler);
+      firebase.database().ref("studytime").child(UserCreds.uid).update({
+          time: firebase.database.ServerValue.increment(clock - time)});
+    }
+    Swal.fire({
+                    html: `<span class="spinner-border text-primary spinner-border-sm" role="status" aria-hidden="true"></span> loading...`,
+                    showConfirmButton:false,
+                });
+    setTimeout(() => {
+      window.location.href = "/pages/studygalaxy.html";
+    }, 1000);
+  });
+  document.getElementById("name").innerHTML += `${UserInfo.Name}`;
+  function isScreenLockSupported() {
+ return ('wakeLock' in navigator);
+}let screenLock;
+navigator.wakeLock.request('screen')
+   .then(lock => { 
+     screenLock = lock; 
+});
+  async function getScreenLock() {
+  if(isScreenLockSupported()){
+    let screenLock;
+    try {
+       screenLock = await navigator.wakeLock.request('screen');
+       console.log("success")
+    } catch(err) {
+       console.log(err.name, err.message);
+    }
+    return screenLock;
+  }
+}
